c5ab7a35ae0cbbc3d7c0d496b2d47d4cd7e73653
Alexander Vassilevski <alexander.vassilevski@gmail.com>
2022-10-11 23:29:37 -0700

    made improvements for singular and plural coin name, fixed missing nickels case, fixed issue where with 0 coins of particular type it would still show it, stopped showing cents, added case for more than 10 dollars and for 0 dollars

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -2,12 +2,50 @@
 /** Exercise 01 - Coins * */
 
 const calculateChange = (input) => {
+  let ret = `$${input} => `;
   const dollars = Math.floor(input);
-  const cents = Math.round((input - dollars) * 100);
-  const quarters = Math.floor(cents / 25);
-  const dimes = Math.floor((cents - quarters * 25) / 10);
-  const pennies = Math.floor(cents - quarters * 25 - dimes * 10);
-  return `$${input} => ${dollars} dollars, ${cents} cents, ${quarters} quarters, ${dimes} dimes, ${pennies} pennies`;
+  if (dollars > 10) {
+    ret += 'Error: the number is too large';
+  } else {
+    const cents = Math.round((input - dollars) * 100);
+    const quarters = Math.floor(cents / 25);
+    const dimes = Math.floor((cents - quarters * 25) / 10);
+    const nickels = Math.floor((cents - quarters * 25 - dimes * 10) / 5);
+    const pennies = Math.floor(cents - quarters * 25 - dimes * 10 - nickels * 5);
+    const changeParts = [];
+    let curDesc = '';
+    if (dollars > 0) {
+      curDesc = 'dollar';
+      if (dollars > 1) curDesc += 's';
+      changeParts.push(`${dollars} ${curDesc}`);
+    }
+    if (quarters > 0) {
+      curDesc = 'quarter';
+      if (quarters > 1) curDesc += 's';
+      changeParts.push(`${quarters} ${curDesc}`);
+    }
+    if (dimes > 0) {
+      curDesc = 'dime';
+      if (dimes > 1) curDesc += 's';
+      changeParts.push(`${dimes} ${curDesc}`);
+    }
+    if (nickels > 0) {
+      curDesc = 'nickel';
+      if (nickels > 1) curDesc += 's';
+      changeParts.push(`${nickels} ${curDesc}`);
+    }
+    if (pennies > 0) {
+      curDesc = 'penny';
+      if (pennies > 1) curDesc = 'pennies';
+      changeParts.push(`${pennies} ${curDesc}`);
+    }
+    if (changeParts.length === 0) {
+      ret += 'Error: no change, no money given';
+    } else {
+      ret += changeParts.join(', ');
+    }
+  }
+  return ret;
 };
 
 // Sample Test Cases
@@ -19,3 +57,5 @@ console.log(calculateChange(0.16));
 // $0.16 ==> 1 dime, 1 nickel, 1 penny
 console.log(calculateChange(15.11));
 // $15.11 ==> Error: the number is too large
+console.log(calculateChange(0.0));
+// $15.11 ==> Error: no change, no money given
